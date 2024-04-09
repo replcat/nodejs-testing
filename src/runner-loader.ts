866b6b8f5bf199b79c9d8a5a160ec0c401a8060d
replcat <replcat@imap.cc>
2024-04-09 18:07:39 +1000

    Detect pre-rendered representations of values in diff fields

--- src/runner-loader.ts
+++ src/runner-loader.ts
@@ -1,6 +1,7 @@
 import * as inspector from "node:inspector";
 import type { TestEvent } from "node:test/reporters";
 import { isatty } from "node:tty";
+import { inspect } from "node:util";
 import { isMainThread } from "node:worker_threads";
 import {
   format as prettyFormat,
@@ -83,6 +84,96 @@ for (const channel of ["stderr", "stdout"] as const) {
   });
 }
 
+const RE_PRERENDERED_DIFF_LINE = /^\s*(actual|expected):\s*'(.+)',?$/gm;
+
+const RE_RAW_ESCAPE_SEQUENCE = /(?<!\\)\\(u\{[^}]+\}|u[0-9A-Fa-f]{4}|x[0-9A-Fa-f]{2}|.)/g;
+
+const simpleEscapeSequences: Record<string, string> = {
+  "n": "\n",
+  "r": "\r",
+  "t": "\t",
+  "b": "\b",
+  "f": "\f",
+  "v": "\v",
+  "0": "\0",
+};
+
+function rawEscapeSequenceToBytes(seq: string) {
+  const [_backslash, first] = seq;
+  const rest = seq.slice(2);
+
+  if (first in simpleEscapeSequences) {
+    return simpleEscapeSequences[first] + rest;
+  }
+
+  if (first === "x") {
+    return String.fromCharCode(parseInt(rest, 16));
+  }
+
+  if (first === "u") {
+    return (rest.startsWith("{"))
+      ? String.fromCodePoint(parseInt(rest.slice(1, -1), 16))
+      : String.fromCharCode(parseInt(rest, 16));
+  }
+
+  // maybe unreachable? but no obvious way to handle it at the moment
+  return first + rest;
+}
+
+/**
+ * Detect whether the assertion library has rendered the diff to a string, and
+ * if so, extract the expected and actual values without unecessary outer quotes
+ * or additional levels of escaping (... or else return undefined).
+ * 
+ * NOTES:
+ * 
+ * This seems unecessarily complicated (there are arguments to pretty-format
+ * which would deal with the escaping issue, for example), but the problem is
+ * that at this point it's really difficult to tell whether these properties are
+ * string representations of other types, or just... strings.  And if they don't
+ * correspond to simple strings in the test cases, then we want to trim off the
+ * unecessary quotes before rendering the diff.
+ * 
+ * So the solution here is to render the error to a string and then extract the
+ * inner string value from that. This has the unfortunate consequence of losing
+ * the quotes around string-to-string comparisons, but fixes all other cases.
+ * 
+ * This is arguably an upstream problem and assertion libraries shouldn't be
+ * rendering these things to strings in the first place, BUT... if you think
+ * about the hoops that are being jumped through elsewhere in this file to get
+ * pretty diffs, now that node has its own no-frills runner, libraries doing
+ * this work themselves starts to seem reasonable? 
+ * 
+ * (i.e. the diffs should be able to show IN THE LIBRARY'S OWN TERMS why the
+ * comparison was rejected, instead of showing a pretty diff with no actual
+ * difference between the rendered expected and actual values, and leaving the
+ * user scratching their head. Probably needs a convention where assertion
+ * libraries add their own render functions to errors or something...)
+ */
+function extractPrerenderedDiff(cause: { expected: unknown, actual: unknown }) {
+  if (typeof cause.actual !== "string" || typeof cause.expected !== "string") return
+
+  // Extract the inner string value of the rendered diff properties of a string
+  // representation of the assertion error... i.e. get lines like `actual:
+  // '"Whatever {}"' (note the nested quoting) and extract new properties like
+  // `{ actual: "Whatever {}" }` with one level of escaping removed.
+  const result: Record<string, string> = {};
+  for (const [_, key, value] of inspect(cause).matchAll(RE_PRERENDERED_DIFF_LINE)) {
+    result[key] = value
+      // replace raw singly-escaped sequences with their corresponding bytes
+      .replaceAll(RE_RAW_ESCAPE_SEQUENCE, seq => rawEscapeSequenceToBytes(seq))
+      // pop one level of escaping on whatever's left
+      .replaceAll("\\\\", "\\");
+  }
+
+  if (result.actual !== undefined && result.expected !== undefined) {
+    return {
+      expected: result.expected,
+      actual: result.actual,
+    }
+  }
+}
+
 module.exports = async function* reporter(source: AsyncGenerator<TestEvent>) {
   for await (const evt of source) {
     if (evt.type === "test:fail") {
@@ -92,15 +183,21 @@ module.exports = async function* reporter(source: AsyncGenerator<TestEvent>) {
         (err.cause as any)._stack = err.stack ? parse(err.stack) : undefined;
       }
 
-      if (err.cause?.hasOwnProperty("expected") && err.cause?.hasOwnProperty("actual")) {
-        let actual = prettyFormat(err.cause.actual, FORMAT_OPTIONS);
-        let expected = prettyFormat(err.cause.expected, FORMAT_OPTIONS);
-        if (actual === expected) {
-          actual = prettyFormat(err.cause.actual, FALLBACK_FORMAT_OPTIONS);
-          expected = prettyFormat(err.cause.expected, FALLBACK_FORMAT_OPTIONS);
+      if (err.cause && "expected" in err.cause && "actual" in err.cause) {
+        const prerendered = extractPrerenderedDiff(err.cause);
+        if (prerendered) {
+          err.cause.actual = prerendered.actual;
+          err.cause.expected = prerendered.expected;
+        } else {
+          let actual = prettyFormat(err.cause.actual, FORMAT_OPTIONS);
+          let expected = prettyFormat(err.cause.expected, FORMAT_OPTIONS);
+          if (actual === expected) {
+            actual = prettyFormat(err.cause.actual, FALLBACK_FORMAT_OPTIONS);
+            expected = prettyFormat(err.cause.expected, FALLBACK_FORMAT_OPTIONS);
+          }
+          err.cause.actual = actual;
+          err.cause.expected = expected;
         }
-        err.cause.actual = actual;
-        err.cause.expected = expected;
       }
     }
 
